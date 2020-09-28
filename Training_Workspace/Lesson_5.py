a7f88e45592ce1617e79d9bcdb0b8267e3d67185
Diana <hsiehdf@email.arizona.edu>
2020-09-27 20:23:18 -0700

    training week 5

--- Training_Workspace/Lesson_5.py
+++ Training_Workspace/Lesson_5.py
@@ -32,3 +32,28 @@ print(filename)
 
 avg_monthly_precip = pd.read_cvs(filepath)
 # %%
+data.columns
+#All of the data. Not most helpful, only shows 10 rows (first 5 last 5)
+#Does include number or rows and columns though
+print (data)
+#The first 5 rows
+data.head()
+#Last five rows
+data.tail()
+#Super helpful! Tells us what each column is made up of
+
+data.info()
+
+#normal, tells us the dimensions of array. 
+data.shape
+
+#%%
+#Grouped by months
+data.groupby(["month"])[["flow"]].describe()
+
+data.groupby(["month"])[["flow"]].mean()
+# %%
+y= data.iloc[:, 3]
+print (y)
+# %%
+data[data["flow"]<=64.9 and >=53.1]

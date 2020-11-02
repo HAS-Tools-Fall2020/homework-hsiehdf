510a1506d18628109a567f0dc9b990dd97a3dd16
Diana <hsiehdf@email.arizona.edu>
2020-11-02 07:35:39 -0700

    Lesson 9

--- Training_Workspace/Lesson_9.py
+++ Training_Workspace/Lesson_9.py
@@ -34,3 +34,54 @@ thisdict = {
 }
 thisdict["year"] = 2018
 # %%
+for x in thisdict:
+    print(thisdict[x])
+# %%
+for x in thisdict.values():
+  print(x)
+# %%
+for x, y in thisdict.items():
+  print(x, y)
+# %%
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+# %%
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+# %%
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily2 = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+# %%

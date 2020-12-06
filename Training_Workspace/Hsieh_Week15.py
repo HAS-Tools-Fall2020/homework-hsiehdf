8b2ca52bfc34a41e94aab36f3b58789fa9bc00f8
Diana <hsiehdf@email.arizona.edu>
2020-12-05 18:37:01 -0700

    Week15 script

---
+++ Training_Workspace/Hsieh_Week15.py
@@ -0,0 +1,158 @@
+# %%
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import geopandas as gpd
+import fiona
+from sklearn.linear_model import LinearRegression
+from shapely.geometry import Point
+
+def get_mean(days_of_flow):
+    """Function to get my mean from x amount of days ago
+
+    This function will give us the mean flows from the *tail* end of our data.
+    We can ask it to give us last weeks mean (i.e., last 7 days) or however
+    many days we ask it to look back to.
+
+    get_mean is an int or float
+    """
+
+    mymean = np.mean(data.tail(days_of_flow)['flow'])
+    return mymean
+
+
+# Below is the USGS streamgauge website that we will read to predict
+# streamflow
+url = 'https://waterdata.usgs.gov/nwis/dv?cb_00060=on&format=' \
+      'rdb&site_no=09506000&referred_module=sw&period=&begin_date' \
+      '=1989-01-01&end_date=2020-12-5'
+
+
+# Read the data into a pandas dataframe
+data = pd.read_table(url, sep='\t', skiprows=30,
+                     names=['agency_cd', 'site_no', 'datetime', 'flow',
+                            'code'], parse_dates=['datetime'], 
+                     )
+
+# Expand the dates to year month day
+data['year'] = pd.DatetimeIndex(data['datetime']).year
+data['month'] = pd.DatetimeIndex(data['datetime']).month
+data['day'] = pd.DatetimeIndex(data['datetime']).day
+data['dayofweek'] = pd.DatetimeIndex(data['datetime']).dayofweek
+
+# Aggregate flow values to weekly
+flow_weekly = data.resample("W", on='datetime').mean()
+
+
+# Created a for loop to shift/lag my flow data 20 times which also creates
+# new columns (1-20) of the shifted flow data.
+
+for k in range(1, 21):
+    flow_weekly[k] = flow_weekly['flow'].shift(k)
+    
+# We are using the entire data set (1989 forward) and chose to use the first
+# 1200 weeks for my training set. I dropped the first twenty weeks since
+# they didn't have lagged data to go with them. My test set is composed
+# of rows 1200 onward.
+
+shifts = list(range(1,21))
+shifts_and_flow = ["flow"] + shifts
+training_start = 21
+training_end = 1200
+
+train = flow_weekly[training_start:training_end][shifts_and_flow]
+test = flow_weekly[training_end:][shifts_and_flow]
+
+# This fit my data into a linear regressoin model. My x axis is my shifted flow
+# values. My y axis is the flow values.
+
+model = LinearRegression()
+
+x = train[shifts]
+y = train['flow'].values
+model.fit(x, y)
+r_sq = model.score(x, y)
+print('The coefficient of determination is:', np.round(r_sq, 2))
+print('The Y intercept is:', np.round(model.intercept_, 2))
+print('The slopes are:', np.round(model.coef_, 2))
+
+
+# Prediction for week 1 using my AR model. I used a for loop that would make a
+# line with the equation y = m1x1 + m2x2 ... m20x20 + b
+
+this_week_ARpred = model.intercept_
+
+for f in range(1, 21):
+    this_week_ARpred = model.coef_[f-1] * test[f].tail(1) + this_week_ARpred
+print("this is the first week AR prediction:", np.int(this_week_ARpred.values))
+
+# Prediction for week 2 using my model. This code uses the weekly flow value 
+# predicted above and uses it in the second weeks prediction.
+# This is achieved by first setting "next_week_pred" equal to
+# model.intercept_, then adding m1 (model.coef_[0]) times x1 (this_week_pred).
+# This gave me y= m1x1 + b. Then I only made the for loop go 1-20 (instead of
+# 1-21) because my first week was already acounted for. This gave me the
+# same equation as above y = m1x1 + m2x2 ... m20x20 + b using my newest
+# weekly flow and all other x values shifted by one week.
+
+next_week_ARpred = model.intercept_
+
+next_week_ARpred = this_week_ARpred * model.coef_[0] + next_week_ARpred
+
+for p in range(1, 20):
+    next_week_ARpred = model.coef_[p] * test[p].tail(1) + next_week_ARpred
+
+print("this is the second week AR prediction:", np.int(next_week_ARpred))
+
+
+# Prediction for week 1 using my AR model. I used a for loop that would make a
+# line with the equation y = m1x1 + m2x2 ... m20x20 + b
+
+this_week_ARpred = model.intercept_
+
+for f in range(1, 21):
+    this_week_ARpred = model.coef_[f-1] * test[f].tail(1) + this_week_ARpred
+print("this is the first week AR prediction:", np.int(this_week_ARpred.values))
+
+# Prediction for week 2 using my model. This code uses the weekly flow value 
+# predicted above and uses it in the second weeks prediction.
+# This is achieved by first setting "next_week_pred" equal to
+# model.intercept_, then adding m1 (model.coef_[0]) times x1 (this_week_pred).
+# This gave me y= m1x1 + b. Then I only made the for loop go 1-20 (instead of
+# 1-21) because my first week was already acounted for. This gave me the
+# same equation as above y = m1x1 + m2x2 ... m20x20 + b using my newest
+# weekly flow and all other x values shifted by one week.
+
+next_week_ARpred = model.intercept_
+
+next_week_ARpred = this_week_ARpred * model.coef_[0] + next_week_ARpred
+
+for p in range(1, 20):
+    next_week_ARpred = model.coef_[p] * test[p].tail(1) + next_week_ARpred
+
+print("this is the second week AR prediction:", np.int(next_week_ARpred))
+
+
+# We will use the mean flow values from the past 4 days to determine next
+# weeks flow. This is because flow appear to be dropping so I want to
+# use the data from the most recent days. Also, based on pressure data I
+# not expecting any rain. 
+
+week1_pred = int(get_mean(4))
+print("my actual week 1 prediction is", week1_pred)
+
+# I'm assuming my week 2 prediction will be slightly higher based on past
+# trends. Therefore I just added 1 cfs for week 2's prediction. 
+
+week2_pred = week1_pred + 1
+print("my actual week 2 prediction is", week2_pred)
+
+seasonal_prediction = pd.Series([58, 45, 40, 51, 57, 56, 65, 70, 80, 90,
+110, 115, 700, 300, 400, 400], index= ["lt_week1", "lt_week2", "lt_week3", "lt_week4",
+"lt_week5", "lt_week6", "lt_week7", "lt_week8", "lt_week9", "lt_week10", 
+"lt_week11", "lt_week12" , "lt_week13", "lt_week14" , "lt_week15", "lt_week16"])
+print (seasonal_prediction)
+
+#%%
